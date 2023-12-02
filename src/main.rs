9f043f687e20510f94d23912ce70b09ab90128bb
Alecto Irene Perez <perez.cs@pm.me>
2023-12-01 22:58:03 -0700

    Clean up implementation of day 1

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,29 @@ use std::io::stdin;
 fn is_digit(ch: u8) -> bool {
     '0' as u8 <= ch && ch <= '9' as u8
 }
+
 fn to_digit(ch: u8) -> u64 {
     (ch - '0' as u8) as u64
 }
 
+fn get_digit(s: &[u8]) -> Option<u64> {
+    let first = *s.first()?;
+    is_digit(first).then(|| to_digit(first))
+}
+
 fn try_get_english_digit(s: &[u8]) -> Option<u64> {
-    return Some(if s.starts_with(b"one") {
-        1
-    } else if s.starts_with(b"two") {
-        2
-    } else if s.starts_with(b"three") {
-        3
-    } else if s.starts_with(b"four") {
-        4
-    } else if s.starts_with(b"five") {
-        5
-    } else if s.starts_with(b"six") {
-        6
-    } else if s.starts_with(b"seven") {
-        7
-    } else if s.starts_with(b"eight") {
-        8
-    } else if s.starts_with(b"nine") {
-        9
-    } else {
-        return None;
-    });
+    const DIGITS: [&[u8]; 10] = [
+        b"zero", b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
+    ];
+    DIGITS
+        .iter()
+        .enumerate()
+        .filter_map(|(i, word)| s.starts_with(word).then_some(i as u64))
+        .next()
+}
+
+fn try_get_any_digit(s: &[u8]) -> Option<u64> {
+    get_digit(s).or_else(|| try_get_english_digit(s))
 }
 
 fn first_and_last<T: Clone>(mut it: impl Iterator<Item = T>) -> Option<(T, T)> {
@@ -37,28 +34,21 @@ fn first_and_last<T: Clone>(mut it: impl Iterator<Item = T>) -> Option<(T, T)> {
 
     return Some((first, last));
 }
-fn get_entry(x: &[u8]) -> u64 {
-    let size = x.len();
-    let digits = (0..size)
+
+fn get_entry(x: &[u8]) -> Option<u64> {
+    let digits = (0..x.len())
         .into_iter()
         .map(|i| x[i..].as_ref())
-        .filter_map(|substr| {
-            if is_digit(substr[0]) {
-                return Some(to_digit(substr[0]));
-            }
-            try_get_english_digit(substr)
-        });
-    let (first_digit, last_digit) =
-        first_and_last(digits).expect("Expected string to contain at least one digit");
+        .filter_map(try_get_any_digit);
+    let (first, last) = first_and_last(digits)?;
 
-    10 * first_digit + last_digit
+    Some(10 * first + last)
 }
 fn main() {
     let result = stdin()
         .lines()
-        .map(|x| {
-            return get_entry(x.expect("Error reading standard input").as_bytes());
-        })
+        .map(Result::unwrap)
+        .filter_map(|s| get_entry(s.as_bytes()))
         .sum::<u64>();
 
     println!("{result}");
